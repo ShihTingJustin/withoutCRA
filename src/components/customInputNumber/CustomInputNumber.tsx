31fb144bc2c99db569b9c6530248277703aeb19d
ShihTingJustin <justinhuang777@gmail.com>
2022-09-09 01:07:39 +0800

    feat: add CustomInputNumber component

---
+++ src/components/customInputNumber/CustomInputNumber.tsx
@@ -0,0 +1,52 @@
+import React, { useState, useRef } from 'react';
+
+import Button from '../button/Button';
+
+import './customInputNumber.scss';
+
+type ValueType = string | number;
+interface InputNumberProps<T extends ValueType = ValueType>
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'value' | 'onChange'> {
+  min?: number;
+  max?: number;
+  step?: number;
+  name?: string;
+  value?: number;
+  disabled?: boolean;
+  className?: string;
+  onBlur?: React.FocusEventHandler<HTMLSpanElement>;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const CustomInputNumber = React.forwardRef(
+  (props: InputNumberProps, ref: React.Ref<HTMLInputElement>) => {
+    const { min, max, step, value = '', disabled, onChange, onBlur, ...inputProps } = props;
+
+    const inputRef = useRef<HTMLInputElement>(null);
+    React.useImperativeHandle(ref, () => inputRef.current);
+
+    const [stateValue, setValue] = useState<ValueType>(value);
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const notNumber = Number.isNaN(Number(e.target.value));
+      if (notNumber) return e.preventDefault();
+
+      onChange(e);
+      setValue(e.target.value);
+    };
+
+    return (
+      <div className="input-number-root">
+        <Button>
+          <span>-</span>
+        </Button>
+        <input value={stateValue} ref={inputRef} onChange={handleChange} {...inputProps} />
+        <Button>
+          <span>+</span>
+        </Button>
+      </div>
+    );
+  }
+);
+
+export default CustomInputNumber;
